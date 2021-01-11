53f8cf29646ed38044dc1314740a6c2e1fb3e0fa
Johann Alberts <alberts.johann@gmail.com>
2021-01-11 13:28:41 +0000

    Call calculate_mars_weight in main function to print the returned weight to the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", calculate_mars_weight(90.5));
+
 }
 
-fn caluculate_mars_weight(weight: f32) -> f32 {
+fn calculate_mars_weight(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
