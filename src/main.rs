e63ccec82d436ea3962d7fa0cbd85f6595e02407
Johann Alberts <alberts.johann@gmail.com>
2021-01-13 09:16:55 +0000

    Create input variable, read from standar input and store value in variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::io;
+
 fn main() {
-    let mut mars_weight = calculate_mars_weight(90.5);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    let mut mars_weight = calculate_mars_weight(100.0);
     mars_weight = mars_weight * 1000.0;
     println!("Weight on Mars: {}g", mars_weight);
 
