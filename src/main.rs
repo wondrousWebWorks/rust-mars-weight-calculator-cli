ac2b5bb6bd2827ab15613a3d29cdc2aa30c9a876
Johann Alberts <alberts.johann@gmail.com>
2021-01-13 09:30:30 +0000

    Parse input value to float and use in function to calculate weight

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
+    print!("Enter weight in kilograms: ");
+    io::stdout().flush().unwrap();
+    
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
-    let mut mars_weight = calculate_mars_weight(100.0);
-    mars_weight = mars_weight * 1000.0;
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+
+    let mars_weight = calculate_mars_weight(weight);
     println!("Weight on Mars: {}g", mars_weight);
 
 }
